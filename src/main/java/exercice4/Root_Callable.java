55b216906019f445c8e316092111c8ee939fe958
Amjad <amjadelassouli@gmail.com>
2026-03-02 21:30:01 +0000

    final changes

--- src/main/java/exercice4/Root_Callable.java
+++ src/main/java/exercice4/Root_Callable.java
@@ -6,18 +6,23 @@ import java.util.concurrent.*;
 public class Root_Callable {
     public static void main(String[] args) throws Exception {
         ExecutorService executor = Executors.newFixedThreadPool(5);
-        List<Future<List<String>>> listeFutures = new ArrayList<>();
+        List<Future<String>> listeFutures = new ArrayList<>();
         double startTime = System.currentTimeMillis();
 
-        for (int i = 0; i <= 5000; i ++) {
-            listeFutures.add(executor.submit(new RootTask(i, Math.min(i + 99, 5000))));
+        for (int i = 0; i <= 5000; i++) {
+            final int n = i;
+            listeFutures.add(executor.submit(new Callable<String>() {
+                @Override
+                public String call() throws Exception {
+                    return "Racine de " + n + " = " + Math.sqrt(n);
+                }
+            }));
         }
-        for (Future<List<String>> future : listeFutures) {
-            List<String> res = future.get();
-            for (String s : res) System.out.println(s);
+        for (Future<String> future : listeFutures) {
+            System.out.println(future.get());
         }
         executor.shutdown();
-        double endTime = System.currentTimeMillis();
+        long endTime = System.currentTimeMillis();
         System.out.println("Temps d'exécution Callable : " + (endTime - startTime) + " ms");
     }
 }
